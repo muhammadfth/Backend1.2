5a6a5ed76ef81504338d8301086832f0c31e255c
muhammadf4th <mhmmdf4th@gmail.com>
2023-12-04 22:26:52 +0700

    week-9

---
+++ task/data/fruits.js
@@ -0,0 +1,11 @@
+/**
+ * TODO 1:
+ * - Buat array yang berisi data buah.
+ * - Refactor variable ke ES6 Variable.
+ */
+let fruits = ["Durian", "Rambutan", "Leci", "Apel"];
+// bisa gunakan const atau let 
+
+
+// TODO 2: export variable fruits
+module.exports = fruits;
